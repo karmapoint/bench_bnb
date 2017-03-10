3e5f5b600e7795731eaae800920e32f8e31e8ea7
Brent <karmapoint@gmail.com>
2017-03-10 12:06:08 -0800

    complete backend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token
+  end
+
+  def sign_out
+    current_user.reset_session_token
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    render json: {base: ['invalid credentials']}, staus 401 if !current_user
+  end 
+
 end
